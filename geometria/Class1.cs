0d6cf1d17a27d71a8b0af7ed2d1502e98d95119d
Cupsuki001 <124387400+Cupsuki001@users.noreply.github.com>
2024-02-02 11:36:56 -0600

    Add files via upload

---
+++ geometria/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometria
+{
+    internal class Class1
+    {
+        double l , an;
+
+        public Class1() { }
+
+        public Class1(double l , double an) 
+        {
+          this.l = l;
+        }
+        public void opc() 
+        {
+            Console.WriteLine("FIGURAS GEOMETRICAS");
+            Console.WriteLine("1.op Cuadrado");
+            Console.WriteLine("2.op Rectangulo");
+            Console.WriteLine("3.op Triangulo");
+            Console.WriteLine("salir con 0");
+            Console.WriteLine("--------------------");
+        }
+
+        public double cuadr(double l) 
+        {
+            return l * l;
+        }
+
+        public double rect(double l , double an) 
+        {
+            return l * an;
+        }
+
+        public double trian(double l , double an) 
+        {
+            return l * an / 2;
+        } 
+
+        public int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.Write(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("////////////////////////////////////");
+                    Console.WriteLine("No se aceptan letras", error.Message);
+                    Console.WriteLine("////////////////////////////////////");
+                   
+                }
+            } while (v == 0);
+            return num;
+        }
+
+    }
+}
---
+++ geometria/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using geometria;
+
+public class figura 
+{
+  public static void Main(string [] args) 
+  {
+        Class1 f = new Class1();
+        int op , op1;
+        double l , ta , an;
+
+        do 
+        {
+            do
+            {
+                Console.WriteLine("//////////////////////");
+                f.opc();
+                op = f.validar("Seleccione una opcion: ");
+                Console.WriteLine("//////////////////////");
+
+            } while (op < 0 || op > 3);
+
+            switch (op) 
+            {
+               case 1: 
+                    {
+                        do
+                        {
+                            Console.WriteLine("-----------------------------");
+                            l = f.validar("Ingrese el lado del cuadrado: ");
+                            Console.WriteLine("//////////////////////");
+
+                        } while (l <= 0);
+                        ta = f.cuadr(l);
+                        Console.WriteLine("El area del cuadrado es de :" + ta);
+                    }
+                    break;
+                case 2:
+                    {
+                        do
+                        {
+                            Console.WriteLine("-----------------------------");
+                            l = f.validar("Ingrese el largo: ");
+                            an = f.validar("Ingrese el ancho: ");
+                            Console.WriteLine("//////////////////////");
+
+                        } while (l <= 0 || an <= 0);
+                        ta = f.rect(l , an);
+                        Console.WriteLine("El area del rectangulo es de :" + ta);
+                    }
+                    break;
+                case 3:
+                    {
+                        do
+                        {
+                            Console.WriteLine("-----------------------------");
+                            l = f.validar("Ingrese la base: ");
+                            an = f.validar("Ingrese la altura: ");
+                            Console.WriteLine("//////////////////////");
+
+                        } while (l <= 0 || an <= 0);
+                        ta = f.trian(l, an);
+                        Console.WriteLine("El area del triangulo es de :" + ta);
+                    }
+                    break;
+                default: 
+                    {
+                        Console.WriteLine("buen dia");
+                    }
+                    break;
+
+
+                   
+            }
+
+
+        } while (op == 4);
+       
+  }
+}
